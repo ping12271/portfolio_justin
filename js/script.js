91f19db43b1e6ce780a99c56ef2dd5aed715c2b0
Hyeeun Shin <hyeeunshin@Hyeeunui-iMac.local>
2020-08-14 01:17:39 +0900

    디테일수정

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-
-// ready 함수 - dom이 렌더링 된후에 마지막으로 실행된다.
-// dom 이란 document object model로 html을 말한다.
 $(function () {
     portfolio.init();
 })
 
-
-var portfolio = {
+const portfolio = {
     init: function () {
         portfolio.handleSideMenu();
         portfolio.getScroll();
@@ -16,18 +12,13 @@ var portfolio = {
     },
 
     handleSideMenu: function () {
-        // 시나리오
-        // $() 셀렉터 함수는 css셀렉터를 이용하여 dom을 선택할수 있다
-        // 버튼메뉴를 선택해서 click이벤트가 일어났을때
-        // html을 선택해서 addClass를 해준다. 구분점이 되는.
-        // $함수는 제이쿼리 오브젝트를 리턴한다.
         $(".btn-menu").on("click", function () {
             $("html").addClass("open-side-menu");
         })
 
         $(".screen").on("click", function () {
             $("html").removeClass("open-side-menu");
-        })
+        });
     },
 
     getScroll: function () {
@@ -51,15 +42,15 @@ var portfolio = {
             } else {
                 $("html").removeClass("need-back-top");
             }
-        })
+        });
     },
 
     backTop: function () {
         $('.back-top').on('click', function () {
             $('html, body').animate({
-                scrollTop:0
+                scrollTop: 0
             })
-        })
+        });
     },
 
     scrollTo: function () {
@@ -69,29 +60,63 @@ var portfolio = {
             const sectionOffset = $('.scroll').eq(menuIndex).offset();
             const offsetTop = sectionOffset.top;
             $('html, body').animate({
-                scrollTop: offsetTop -60
+                scrollTop: offsetTop
             }, 600)
-        })
+        });
     },
 
     slide: function () {
-        $('.testimonial-slideshow').slick({
+        $('.testimonial-slide').slick({
             prevArrow: $(".slick-prev"),
             nextArrow: $(".slick-next")
-        }),
-            $('.blog-slideshow').slick({
-                infinite: true,
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                arrows: false,
-                dots: true
-            }),
-            $('.gallery-slideshow').slick({
-                centerMode: true,
-                centerPadding: '80px',
-                slidesToShow: 3,
-                arrows: false
-            });
+        });
+
+        $('.blog-slide, .portfolio-slide').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            arrows: false,
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                },
+            ]
+        });
+        $('.portfolio-slide').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            arrows: false,
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                },
+            ]
+        });
     }
 
 }
